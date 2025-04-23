0ad37dfe3779688f1fbdb731854aa73f0165c45d
Jairam Deo <jairamdeo2002@gmail.com>
2025-04-23 18:38:11 +0530

    Home Section 3 Featured collection design updated

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import BannerCarousel from './BannerCarousel'
 import HomeSection2 from './HomeSection2'
+import HomeSection3 from './HomeSection3'
+import HomeSection4 from './HomeSection4';
 
 export default function Home() {
   return (
     <div>
       <BannerCarousel/>
       <HomeSection2/>
+      <HomeSection3/>
+      <HomeSection4/>
     </div>
   )
 }
---
+++ client/src/Pages/Home/HomeSection4.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function HomeSection4() {
+  return (
+    <div>HomeSection4</div>
+  )
+}
